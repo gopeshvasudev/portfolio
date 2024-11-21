04f06e181ebacb01b649be9831a32e5f272c8fd9
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-11-21 20:02:04 +0530

    About page added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,7 @@ import MainContainer from "./MainContainer";
 
 const Body = () => {
   return (
-    <main className="main relative w-full bg-zinc-900 text-white flex flex-col md:flex-row px-5 lg:px-12 gap-5">
+    <main className="main relative w-full bg-zinc-900 text-white flex flex-col md:flex-row px-5 lg:px-8 gap-16">
       <MyInfoCard />
       <MainContainer />
     </main>
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,7 +1,44 @@
+import { PiHouseLineBold } from "react-icons/pi";
+import PageTitle from "./PageTitle";
+import { FaRegUser } from "react-icons/fa";
+
 const MainContainer = () => {
   return (
-    <div className="container w-full md:w-[72%] border">
-      <section className="page1 w-full h-screen"></section>
+    <div className="container w-full md:w-[72%]">
+      <section className="page1 w-full h-screen flex flex-col justify-evenly items-center md:items-start">
+        <PageTitle titleName="Introduce" titleIcon={<PiHouseLineBold />} />
+
+        <h1 className="text-4xl text-center md:text-start md:text-[5.5vw] leading-[1.1] font-light">
+          Say Hi from <span className="text-green-400 font-light">Gopesh</span>,{" "}
+          <br />
+          Frontend & Full Stack <br />
+          Developer
+        </h1>
+
+        <p className="font-light text-center md:text-start text-zinc-300">
+          I design and code beautifully simple things and I love what i do.{" "}
+          <br />
+          Just simple like that!
+        </p>
+      </section>
+
+      <section className="page2 w-full h-screen flex flex-col items-center md:items-start justify-evenly">
+        <PageTitle titleName="About" titleIcon={<FaRegUser />} />
+
+        <div>
+          <h2 className="text-3xl md:text-[4vw] text-center md:text-start font-light mb-10 leading-[1.2]">
+            Building Digital Experiences <br /> That{" "}
+            <span className="text-green-400 font-light">Matter</span>
+          </h2>
+
+          <p className="px-3 sm:px-10 md:px-0 md:w-8/12 text-center md:text-start leading-[2] font-light text-zinc-300">
+            As a passionate frontend and full-stack developer, I craft seamless
+            digital experiences, blending clean code with intuitive design. I
+            thrive on solving challenges, learning new technologies, and
+            delivering impactful solutions.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
