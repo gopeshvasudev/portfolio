01316d9f456c0c3ccc808c509cce58946d46b57c
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-11-22 22:26:46 +0530

    Added dummy data into constants file

--- src/constants/dummyData.js
+++ src/constants/dummyData.js
@@ -1,2 +1,72 @@
+import { AiFillInstagram } from "react-icons/ai";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import { GrLinkedinOption } from "react-icons/gr";
+import { MdDesignServices } from "react-icons/md";
 
+export const socialMediaIcons = [
+  {
+    href: "#",
+    icon: GrLinkedinOption,
+  },
+  {
+    href: "#",
+    icon: FaGithub,
+  },
+  {
+    href: "#",
+    icon: AiFillInstagram,
+  },
+  {
+    href: "#",
+    icon: FaTwitter,
+  },
+];
 
+export const specializationCardData = [
+  {
+    title: "Creative Design",
+    description: "I creates digital experience modern, animated and smooth",
+    icon: MdDesignServices,
+  },
+  {
+    title: "Creative Design",
+    description: "I creates digital experience modern, animated and smooth",
+    icon: MdDesignServices,
+  },
+  {
+    title: "Creative Design",
+    description: "I creates digital experience modern, animated and smooth",
+    icon: MdDesignServices,
+  },
+];
+
+export const languageCardData = [
+  {
+    imageUrl: "../assets/lang/javascript.webp",
+    text: "Javascript",
+  },
+  {
+    imageUrl: "../assets/lang/react.webp",
+    text: "React",
+  },
+  {
+    imageUrl: "../assets/lang/nodejs.webp",
+    text: "Node js",
+  },
+  {
+    imageUrl: "../assets/lang/express.webp",
+    text: "Express js",
+  },
+  {
+    imageUrl: "../assets/lang/mongodb.webp",
+    text: "Mongo DB",
+  },
+  {
+    imageUrl: "../assets/lang/mysql.webp",
+    text: "My SQL",
+  },
+  {
+    imageUrl: "../assets/lang/gsap.webp",
+    text: "Gsap",
+  },
+];
