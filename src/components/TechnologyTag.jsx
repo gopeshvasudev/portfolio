d95ff9d92ddf72f6189f57537e6fc16f9667b895
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-12-06 14:47:48 +0530

    added technologies in project page

---
+++ src/components/TechnologyTag.jsx
@@ -0,0 +1,7 @@
+const TechnologyTag = ({name}) => {
+  return (
+    <div className="technology-tag py-2 px-4 border-2 border-green-400 rounded-full">{name}</div>
+  );
+};
+
+export default TechnologyTag;
